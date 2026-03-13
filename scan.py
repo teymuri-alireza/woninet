662ac97dca08456d64c1fcdb28e1886b27217037
teymuri-alireza <teymuri.alireza98@gmail.com>
2026-03-13 14:39:00 +0330

    feat: optimise function documents, fix issue and add log error

--- scan.py
+++ scan.py
@@ -5,18 +5,21 @@ rootLogger = logger_function()
 # setting scan files path
 SCAN_FILES = "scan-files"
 
-def scanLogic(ipAddr: str):
+def scanLogic(ipAddr: str) -> int:
     """
     The logic for scanning local network.
 
-    This function calles generateIP for generating 255 IP addreses, then calls
-    pingFunction to check which IP address is up. Then returns the result to server
-    to be sent to JavaScript in the front-end.
+    This function calles generateIP for generating 255 IP addreses (from x.x.x.1 to x.x.x.254),
+    then calls the pingFunction to check which IP address is up. Then returns the result to server (main.py).
 
     Parameters:
-        ipAddr (str) : private IP of the device
+        ipAddr (str) : private IP of the device.
+
+    Returns:
+        int : exit code of the function (0 for success, -1 for failure).
     """
     if ipAddr.strip == "":
+        rootLogger.error("IP address is empty. Quitting scanLogic function.")
         return -1
     exit_code = generateIP(ipAddr)
     if exit_code == 0:
@@ -27,19 +30,19 @@ def scanLogic(ipAddr: str):
 
 def generateIP(ipAddr: str) -> int:
     """
-    Generates 255 IPs on a local network
+    Generates 255 IPs on a local network. (from x.x.x.1 to x.x.x.254).
 
     Parameters:
-        ipAddr (str) : private IP of the device
+        ipAddr (str) : private IP of the device.
 
     Returns:
-        int : exit code of the function (0 for success, -1 for failure)
+        int : exit code of the function (0 for success, -1 for failure).
     """
     try:
         ip_seperate = ipAddr.split(".")
         with open(f"{SCAN_FILES}/ip_list.txt", "w") as file:
             for i in range(1,255):
-                file.write(f"{ip_seperate[0]}.{ip_seperate[1]}.{ip_seperate[2]}.{i}")
+                file.write(f"{ip_seperate[0]}.{ip_seperate[1]}.{ip_seperate[2]}.{i}\n")
         return 0
     except Exception as e:
         rootLogger.error(f"Error at generateIP function in scan.py file: {e}")
