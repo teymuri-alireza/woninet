8ed0593756b7656aeceaaf62d3918bc3aa8936a1
teymuri-alireza <teymuri.alireza98@gmail.com>
2026-03-20 14:28:01 +0330

    feat(log-output): remove file logging & only keep stdout logging

--- utilities/logger.py
+++ utilities/logger.py
@@ -12,48 +12,24 @@ def logger_function() -> logging.Logger:
     This function uses datetime.datetime.now and a list of month names
     to indicate the current month.
 
-    The log output destination will be fethed from settings.json file.
-
-    1 means stdout only, 2 means file only, 3 means stdout and file.
+    The default output is console (stdout) and the level is info.
     """
     monthNumber = datetime.datetime.now()
     monthList = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
     monthValue = monthList[monthNumber.month - 1]
 
-    fileLogFormat = "%(asctime)s - %(levelname)s - %(message)s"
     consoleLogFormat = "%(asctime)s - %(message)s"
-
     dateFormat = f"[%d/{monthValue}/%Y %H:%M:%S]"
+
     rootLogger = logging.getLogger()
 
     if not rootLogger.handlers:
-        try:
-            # read settings.json
-            with open(f"{SCRIPT_PATH}/settings.json", "r") as file:
-                settings = json.load(file)
-            log_settings = settings["log_output"]
 
-            if log_settings in [2, 3]:
-                try:
-                    fileHandler = logging.FileHandler(filename=f"{SCRIPT_PATH}/logs.txt")
-                    fileHandler.setFormatter(logging.Formatter(fmt=fileLogFormat, datefmt=dateFormat))
-                    fileHandler.setLevel(logging.DEBUG)
-                    rootLogger.addHandler(fileHandler)
+        consoleHandler = logging.StreamHandler()
+        consoleHandler.setFormatter(logging.Formatter(fmt=consoleLogFormat, datefmt=dateFormat))
+        consoleHandler.setLevel(logging.INFO)
+        rootLogger.addHandler(consoleHandler)
 
-                except PermissionError:
-                    print("Are you root?")
-                    exit(1)
-            
-            if log_settings in [1, 3]:
-                consoleHandler = logging.StreamHandler()
-                consoleHandler.setFormatter(logging.Formatter(fmt=consoleLogFormat, datefmt=dateFormat))
-                consoleHandler.setLevel(logging.INFO)
-                rootLogger.addHandler(consoleHandler)
+        rootLogger.setLevel(logging.INFO)
 
-            rootLogger.setLevel(logging.DEBUG)
-        
-        except json.JSONDecodeError as e:
-            rootLogger.error(f"Error at logger.py: {e}")
-            exit(1)
-    
     return rootLogger
