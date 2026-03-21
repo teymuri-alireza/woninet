8f2093f39eb1845e05f69f6cd4c833c4dd5ca22a
teymuri-alireza <teymuri.alireza98@gmail.com>
2026-03-21 15:14:49 +0330

    feat(logging-format): simplify logging format

--- utilities/logger.py
+++ utilities/logger.py
@@ -1,32 +1,22 @@
-import json
 import logging
-import datetime
+
 # global variables
 SCRIPT_PATH = "/usr/local/lib/.pymonitor"
 
 def logger_function() -> logging.Logger:
     """
-    Uses a logging format similar to the method send_request() in the class
-    http.server.SimpleHTTPRequestHandler
-
-    This function uses datetime.datetime.now and a list of month names
-    to indicate the current month.
+    Returns the rootLogger, which uses a simple format for logging.
 
     The default output is console (stdout) and the level is info.
     """
-    monthNumber = datetime.datetime.now()
-    monthList = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-    monthValue = monthList[monthNumber.month - 1]
-
-    consoleLogFormat = "%(asctime)s - %(message)s"
-    dateFormat = f"[%d/{monthValue}/%Y %H:%M:%S]"
+    consoleLogFormat = "%(message)s"
 
     rootLogger = logging.getLogger()
 
     if not rootLogger.handlers:
 
         consoleHandler = logging.StreamHandler()
-        consoleHandler.setFormatter(logging.Formatter(fmt=consoleLogFormat, datefmt=dateFormat))
+        consoleHandler.setFormatter(logging.Formatter(fmt=consoleLogFormat, datefmt=None))
         consoleHandler.setLevel(logging.DEBUG)
         rootLogger.addHandler(consoleHandler)
 
