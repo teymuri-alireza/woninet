10da170fff374208874750184ee07896dcb225d8
teymuri-alireza <teymuri.alireza98@gmail.com>
2026-05-09 23:23:26 +0330

    refactor: migrate function-based to class-based for more control in database engine.py
    The new class-based code can be initialized in the core/engine.py and
    accepts database_path as argument. This new structure will also prevent
    circular import error.

--- woninet/database/engine.py
+++ woninet/database/engine.py
@@ -1,15 +1,41 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, Engine
+from sqlalchemy.orm import sessionmaker, Session
 from woninet.database.base import Base
 
-engine = create_engine("sqlite:///woninet.db", echo=False, future=True)
-SessionLocal = sessionmaker(bind=engine, autoflush=False)
 
-
-def init_db() -> None:
+class DatabaseEngine:
     """
-    Initialize the database and create missing tables.
+    Encapsulate database engine creation and initialization logic.
     """
-    import woninet.database.tables  # noqa: F401
 
-    Base.metadata.create_all(bind=engine)
+    def __init__(self, database_path: str) -> None:
+        """
+        Create a SQLAlchemy engine and session factory for the given database.
+
+        Args:
+            database_path (str): Path to the SQLite database file.
+        """
+        self.engine = create_engine(
+            f"sqlite:///{database_path}", echo=False, future=True
+        )
+        self.SessionLocal = sessionmaker(bind=self.engine, autoflush=False)
+
+    def init_db(self) -> None:
+        """
+        Initialize the database and create missing tables.
+        """
+        import woninet.database.tables  # noqa: F401
+
+        Base.metadata.create_all(bind=self.engine)
+
+    def get_database_enigne(self) -> Engine:
+        """
+        Return the SQLAlchemy Engine instance.
+        """
+        return self.engine
+
+    def get_session_factory(self) -> sessionmaker[Session]:
+        """
+        Return the SQLAlchemy session factory used to create sessions.
+        """
+        return self.SessionLocal
