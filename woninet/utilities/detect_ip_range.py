b4303115eb5d1c44c19e013d22bb72fd517d8331
teymuri-alireza <teymuri.alireza98@gmail.com>
2026-05-20 10:45:14 +0330

    feat: add detect_ip_range() to detect the form of the provided IP address

---
+++ woninet/utilities/detect_ip_range.py
@@ -0,0 +1,22 @@
+
+def detect_ip_range(ip: str) -> bool:
+    """
+    Detect if an IP address is provided as a range.
+    (e.g., 192.168.10.10-20)
+
+    Args:
+        ip (str): IP address to detect.
+
+    Returns:
+        bool: Boolean indicating if IP address is provided as a range.
+
+    Raises:
+        ValueError: If the range notation is used in any of the first three octets.\n
+            (e.g., 192.168.1-10.10 - 192.168-188.1.10 - 192.180.168.1.10)
+    """
+    base, last_octet = ip.rsplit(".", maxsplit=1)
+    if "-" in base:
+        raise ValueError("Ip addess range is only allowed in the last octet.")
+    if "-" in last_octet:
+        return True
+    return False
