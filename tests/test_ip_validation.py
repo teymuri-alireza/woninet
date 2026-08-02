3e3584c2bbe687c9da8c11aa3d50edfc68e2b1c8
teymuri-alireza <teymuri.alireza98@gmail.com>
2026-08-02 14:47:20 +0330

    refactor: update truth and false checks in test to prevent ruff errors

--- tests/test_ip_validation.py
+++ tests/test_ip_validation.py
@@ -1,7 +1,7 @@
 from woninet.utilities.ip_validator import is_ip_list_valid
 
 def test_valid_ip():
-    assert is_ip_list_valid(ip_list=["192.168.1.1"]) == True
+    assert is_ip_list_valid(ip_list=["192.168.1.1"])
 
 def test_invalid_ip():
-    assert is_ip_list_valid(ip_list=["192.168.1"]) == False
+    assert not is_ip_list_valid(ip_list=["192.168.1"])
